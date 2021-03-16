f8c03bee24b5757279773e38bb1fdf99fedca8a9
traj3ctory <davidbenard.bd@gmail.com>
2021-03-16 08:26:31 +0100

    component to update status

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -9,7 +9,7 @@ import Edit from "./Edit";
 /**
  * @author traj3ctory
  * @function List
- **/ 
+ **/
 
 const List = ({ tasks, deleteTask, completeTask, updateTask }) => {
   const [modalShow, setModalShow] = useState(false);
@@ -17,20 +17,18 @@ const List = ({ tasks, deleteTask, completeTask, updateTask }) => {
   const [newValue, setNewValue] = useState({});
 
   const handleCheck = (e, data) => {
-    e.preventDefault();
-
-    console.log(e.target.checked);
-    return
+    // e.preventDefault();
+// More Work
     if (e.target.checked) {
       let taskId = Math.floor(Math.random() * 1000)
       setNewValue({ id: taskId, taskName: data.taskName, status: 'completed' });
       console.log(newValue);
-      // completeTask(newValue);
+      completeTask(newValue);
     } else {
       let taskId = Math.floor(Math.random() * 1000)
       setNewValue({ id: taskId, taskName: data.taskName, status: 'pending' });
       console.log(newValue);
-      // completeTask(newValue);
+      completeTask(newValue);
     }
   };
 
@@ -39,7 +37,8 @@ const List = ({ tasks, deleteTask, completeTask, updateTask }) => {
     tasks.map((task, id) => {
       return (
         <ListGroup.Item key={task.id} className="del-task">
-          <Form.Check aria-label="Todo App" inline type="checkbox" id={`${task.id}`} onChange={(e) => { handleCheck(e, task); }} />
+          {/* <Form.Check aria-label="Todo App" inline type="checkbox" id={`${task.id}`} onChange={(e) => { handleCheck(e, task); }} /> */}
+          <Form.Check aria-label="Todo App" inline type="checkbox" id={`${task.id}`} onChange={(e) => handleCheck(e, task)} />
           {count++} : {task.taskName}{" "}
           <span className="float-right">
             <button className="btn btn-danger btn-sm" onClick={(e) => { deleteTask(task.id) }}>
